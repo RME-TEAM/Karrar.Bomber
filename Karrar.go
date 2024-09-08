86dbbb3a731bfdffb9bb9c1c6d11daee4977ec5b
Karrar <abol.game.ir2@gmail.com>
2024-09-08 16:19:34 +0330

    Karrar.go

--- Karrar.go
+++ Karrar.go
@@ -62,7 +62,7 @@ func main() {
 	`)
 	var phone string
 	fmt.Println("\033[01;31m[\033[01;32m+\033[01;31m] \033[01;33mSms bomber ! number web service : \033[01;31m177 \n\033[01;31m[\033[01;32m+\033[01;31m] \033[01;33mCall bomber ! number web service : \033[01;31m6\n\n")
-	fmt.Print("\033[01;31m[\033[01;32m+\033[01;31m] \033[01;32mEnter phone [Ex : 09xxxxxxxxxx]: \033[00;36m")
+	fmt.Print("\033[01;31m[\033[01;32m+\033[01;31m] \033[01;32mEnter phone [Ex : 09XXXXXXXXXX]: \033[00;36m")
 	fmt.Scan(&phone)
 
 	var repeatCount int
